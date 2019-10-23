5bdf5e64300ba00c9be4bbfa0c8d58e0433d3f66
Maxime-De-Bruyn <181770@umons.ac.be>
2019-10-23 14:59:45 +0200

    yes

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,14 +1,24 @@
 package exercise1;
+import java.util.*;
 
 public class Exercise1 {
 	static Person createPerson(String name, int age) {
 		// Ajoutez les champs name et age à la classe Person.
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		Person C = new Person(name, age);
+		return C;
 	}
 
 	public static void main(String[] args) {
+		Person A = new Person("loic", 12);
+		A.affiche();
+		System.out.println(A.getName()+" "+A.getAge() );
+		A.setName("michel");
+		A.affiche();
+		Person C = new Person(args[0], Integer.parseInt(args[1]));
+		C.affiche();
+
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,35 @@
 package exercise1;
+import java.util.*;
 
-class Person {
+
+public class Person {
+	private String name;
+	private int age;
+
+	public Person(){
+		System.out.print("Entrez le nom : ");
+		Scanner lectureClavier = new Scanner(System.in);
+		name = lectureClavier.next();
+
+        System.out.print("Entrez l'age : ");
+        age = lectureClavier.nextInt();
+	}
+	public Person(String nom, int Age){
+		name = nom;
+		age = Age;
+	}
+	public String getName(){return name;}
+
+	public void setName(String nom){name = nom;}
+
+	public int getAge() {return age; }
+
+	public void setAge(int Age) {age = Age; }
+
+	public void affiche() {
+		System.out.print("Nom : ");
+		System.out.println(name);
+	    System.out.print("Age : ");
+	    System.out.println(age);
+	}
 }
